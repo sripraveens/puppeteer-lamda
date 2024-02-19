72adfcbcba5dd01b840bbc1bc9e01b2d3573e949
Sri Praveen <sripraveens@yahoo.in>
2024-02-20 00:07:05 +0530

    Initial commit for the screenshot service, working as intented in lamda

---
+++ index.js
@@ -0,0 +1,57 @@
+const chromium = require("chrome-aws-lambda");
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "ap-southeast-1" });
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+  let result = null;
+  let browser = null;
+  let url = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+    // const delayInMilliseconds = 15000;
+
+    // setTimeout(function () {
+    //   console.log("Delay is over!");
+    // }, delayInMilliseconds);
+    await page.goto(event.url || "https://blueprintcoc.com", {
+      waitUntil: "networkidle2",
+    });
+    await page.waitForSelector("canvas");
+
+    await page.evaluate(() => {
+      const canvas = document.querySelector("canvas");
+      canvas.style.left = "-200px";
+    });
+
+    const buffer = await page.screenshot();
+    const params = {
+      Bucket: "take-screeshots-cp",
+      Key: `${Date.now()}.png`,
+      Body: buffer,
+      ContentType: "image/png",
+      ACL: "public-read",
+    };
+
+    result = await s3.putObject(params).promise();
+    url = `https://take-screeshots-cp.s3.ap-southeast-1.amazonaws.com/${params.Key}`;
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return callback(null, url);
+};
